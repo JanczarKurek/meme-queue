0f8924df2408193011d81127b33e621a44f9a1b8
Janczar Knurek <jacek@beit.tech>
2024-08-13 22:33:35 +0200

    Small fixes

--- core/main.py
+++ core/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import pathlib
 
 from monolith.infrastructure import execute_infrastructure
 from resource.resource_queue import SimpleResourceQueue
@@ -11,12 +12,13 @@ logging.basicConfig(level=logging.INFO)
 class ImageFsProvider(FsResourceProvider):
     RESOURCE_TAG = "meme"
 
-    # def next_resource(self) -> asyncio.Coroutine[asyncio.Any, asyncio.Any, dict | None]:
-    #     result = super().next_resource()
-    #     result["payload"] = ""
+    async def next_resource(self):
+        result = await super().next_resource()
+        result["payload"] = "http://internet.www/memy.www/" + pathlib.Path(result["payload"]).name
+        return 
 
 
 if __name__ == "__main__":
     queue = SimpleResourceQueue(10)
-    asyncio.run(execute_infrastructure(queue, (ImageFsProvider(queue, 2., "/mnt/nfs/memy.www"),)))
+    asyncio.run(execute_infrastructure(queue, (ImageFsProvider(queue, 10., "/mnt/nfs/memy.www"),)))
     print(queue._content)
